e9a07b0c262a2af22d972d6fa7fc0d2a7f8b0e50
Miquel van Smoorenburg <mike@langeraar.net>
2023-01-22 14:24:22 +0100

    bla

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,28 @@
 use std::time::Instant;
 use std::fs::File;
 use std::io;
-use read_ranges::{self, ReadRangeBuf, flush};
+use read_ranges::{self, ReadRangeBuf, flush, willneed};
 
 type ReadRangesFn = fn (&File, ReadRangeBuf) -> io::Result<ReadRangeBuf>;
 
-fn bench(filename: &str, reader: ReadRangesFn) -> io::Result<()> {
+fn bench(filename: &str, reader: ReadRangesFn, offset: &mut u64) -> io::Result<()> {
     let file = File::open(filename)?;
     flush(&file)?;
-
-    let mut offset = 0;
 
     for i in 0 .. 10 {
         let mut buf = ReadRangeBuf::default();
         for _ in 0 .. 200 {
-            buf.ranges.push((offset, 640));
-            offset += 68000;
+            // buf.ranges.push((*offset, 640));
+            // *offset += 68000;
+            buf.ranges.push((*offset, 64000));
+            *offset += 64000 + 4200;
         }
+        let n = buf.ranges.len() - 1;
+        let rlen = buf.ranges[n].0 + buf.ranges[n].1 as u64 - buf.ranges[0].0;
+        willneed(&file, buf.ranges[0].0, rlen as usize)?;
         let buf = reader(&file, buf)?;
-        println!("{}: {}", i, buf.buf.len());
-        offset += 6800000;
+        println!("{}: {} (range: {})", i, buf.buf.len(), rlen);
+        *offset += 6800000;
     }
 
     Ok(())
@@ -28,17 +31,18 @@ fn bench(filename: &str, reader: ReadRangesFn) -> io::Result<()> {
 fn main() -> io::Result<()> {
     let mut args = std::env::args();
     let filename = args.nth(1).unwrap();
+    let mut offset = 0;
 
     let start = Instant::now();
-    bench(&filename, read_ranges::read_ranges)?;
-    println!("aio::read_ranges: {:?}", start.elapsed());
-
-    let start = Instant::now();
-    bench(&filename, read_ranges::mmap::read_ranges)?;
+    bench(&filename, read_ranges::mmap::read_ranges, &mut offset)?;
     println!("aio::mmap::read_ranges: {:?}", start.elapsed());
 
     let start = Instant::now();
-    bench(&filename, read_ranges::aio::read_ranges)?;
+    bench(&filename, read_ranges::read_ranges, &mut offset)?;
+    println!("aio::read_ranges: {:?}", start.elapsed());
+
+    let start = Instant::now();
+    bench(&filename, read_ranges::aio::read_ranges, &mut offset)?;
     println!("aio::aio::read_ranges: {:?}", start.elapsed());
 
     Ok(())
--- src/fadvise.rs
+++ src/fadvise.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io;
 use std::os::fd::AsRawFd;
 
-use libc::{c_int, off_t, posix_fadvise, POSIX_FADV_DONTNEED};
+use libc::{c_int, off_t, posix_fadvise, POSIX_FADV_DONTNEED, POSIX_FADV_WILLNEED};
 
 pub fn flush(file: &File) -> io::Result<()> {
     let meta = file.metadata()?;
@@ -10,6 +10,18 @@ pub fn flush(file: &File) -> io::Result<()> {
 
     let res = unsafe {
         posix_fadvise(file.as_raw_fd() as c_int, 0, len as off_t, POSIX_FADV_DONTNEED)
+    };
+
+    if res != 0 {
+        return Err(io::Error::last_os_error());
+    }
+    Ok(())
+}
+
+pub fn willneed(file: &File, start: u64, len: usize) -> io::Result<()> {
+
+    let res = unsafe {
+        posix_fadvise(file.as_raw_fd() as c_int, start as off_t, len as off_t, POSIX_FADV_WILLNEED)
     };
 
     if res != 0 {
